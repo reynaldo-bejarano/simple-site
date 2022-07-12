c4fd8d8264d32272a966e86a8664e0a5a611adbf
reynaldo-bejarano <reynaldo.bjsz@gmail.com>
2022-07-11 22:10:59 -0600

    javascript update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 const toggleMovil = document.getElementById("toggleMovil");
 const navegador = document.getElementById("navegador");
 const hamburger = document.getElementById("hamburger");
+const navItem = document.querySelectorAll(".nav__item");
 
 toggleMovil.addEventListener("click", () => {
   navegador.classList.toggle("navegadorShow");
   hamburger.classList.toggle("hamburgerX");
 });
+
+navItem.forEach((item) =>
+  item.addEventListener("click", () => {
+    navegador.classList.toggle("navegadorShow");
+    hamburger.classList.toggle("hamburgerX");
+  })
+);
